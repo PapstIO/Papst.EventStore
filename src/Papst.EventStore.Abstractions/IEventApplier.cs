ae2b82e54a195a69452eefdc8edc09c192d92778
Marco Papst <marco@papst.io>
2020-06-09 21:20:49 +0200

    fix missing type parameter constraint

--- src/Papst.EventStore.Abstractions/IEventApplier.cs
+++ src/Papst.EventStore.Abstractions/IEventApplier.cs
@@ -9,11 +9,13 @@ namespace Papst.EventStore.Abstractions
     /// <typeparam name="TEntity">The Entity the Event shall be applied</typeparam>
     /// <typeparam name="TEvent">The Event that shall be applied</typeparam>
     public interface IEventApplier<TEntity, TEvent> : IEventApplier<TEntity>
+        where TEntity: class
     {
         Task ApplyAsync(TEvent eventInstance, TEntity entityInstance);
     }
 
     public interface IEventApplier<TEntity>
+        where TEntity: class
     {
         Task ApplyAsync(JObject eventInstance, TEntity entityInstance);
     }
