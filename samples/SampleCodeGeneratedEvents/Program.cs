61f0c21137463dc6d1542e4bfae8c4c8454841c3
Marco Papst <marco@papst.io>
2021-12-21 09:55:27 +0100

    fix sample

--- samples/SampleCodeGeneratedEvents/Program.cs
+++ samples/SampleCodeGeneratedEvents/Program.cs
@@ -17,13 +17,13 @@ public static class Program
     var config = new ConfigurationBuilder()
         .AddInMemoryCollection(new[]
         {
-                    // Endpoint is local emulator
-                    new KeyValuePair<string, string>($"{_section}:Endpoint", "https://localhost:8081"),
-                    new KeyValuePair<string, string>($"{_section}:AccountSecret", "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw=="),
-                    new KeyValuePair<string, string>($"{_section}:InitializeOnStartup", "true"),
-                    new KeyValuePair<string, string>($"{_section}:Database", "EventStoreSample"),
-                    new KeyValuePair<string, string>($"{_section}:Collection", "Events"),
-                    new KeyValuePair<string, string>($"{_section}:StartVersion", "0") // start with version 1 for new streams
+          // Endpoint is local emulator
+          new KeyValuePair<string, string>($"{_section}:Endpoint", "https://localhost:8081"),
+          new KeyValuePair<string, string>($"{_section}:AccountSecret", "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw=="),
+          new KeyValuePair<string, string>($"{_section}:InitializeOnStartup", "true"),
+          new KeyValuePair<string, string>($"{_section}:Database", "EventStoreSample"),
+          new KeyValuePair<string, string>($"{_section}:Collection", "Events"),
+          new KeyValuePair<string, string>($"{_section}:StartVersion", "0") // start with version 1 for new streams
         }).Build();
 
     var serviceProvider = new ServiceCollection()
