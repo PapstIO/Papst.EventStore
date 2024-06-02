1c6b2c6c5283e9d9b9e26c1c8483930a00477bc5
Marco Papst <marco@papst.io>
2024-06-02 14:37:47 +0200

    Add Option to Build Index on Append

--- src/Papst.EventStore.AzureCosmos/CosmosEventStore.cs
+++ src/Papst.EventStore.AzureCosmos/CosmosEventStore.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Azure.Cosmos;
+﻿using System.Net;
+using Microsoft.Azure.Cosmos;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Papst.EventStore.AzureCosmos.Database;
@@ -20,11 +21,19 @@ internal sealed class CosmosEventStore(
   public async Task<IEventStream> GetAsync(Guid streamId, CancellationToken cancellationToken = default)
   {
     Logging.GetEventStream(logger, streamId);
-    ItemResponse<EventStreamIndexEntity>? stream = await dbProvider.Container
-      .ReadItemAsync<EventStreamIndexEntity>(
-        IndexDocumentName,
-        new(streamId.ToString()),
-        cancellationToken: cancellationToken).ConfigureAwait(false);
+    ItemResponse<EventStreamIndexEntity>? stream;
+    try
+    {
+      stream = await dbProvider.Container
+        .ReadItemAsync<EventStreamIndexEntity>(
+          IndexDocumentName,
+          new(streamId.ToString()),
+          cancellationToken: cancellationToken).ConfigureAwait(false);
+    }
+    catch (CosmosException e) when (e.StatusCode == HttpStatusCode.NotFound && options.Value.BuildIndexOnNotFound)
+    {
+      stream = await ReadStreamAndBuildIndex(streamId, cancellationToken).ConfigureAwait(false);
+    }
 
     return new CosmosEventStream(
       loggerFactory.CreateLogger<CosmosEventStream>(),
@@ -33,6 +42,37 @@ internal sealed class CosmosEventStore(
       dbProvider,
       eventTypeProvider,
       idStrategy);
+  }
+
+  private async Task<ItemResponse<EventStreamIndexEntity>> ReadStreamAndBuildIndex(
+    Guid streamId,
+    CancellationToken cancellationToken
+  )
+  {
+    // read the stream latest version
+    QueryDefinition query = new QueryDefinition(
+      "SELECT * FROM c WHERE c.StreamId = @streamId AND c.Version = (SELECT VALUE MAX(c2.Version) FROM c c2 WHERE c2.StreamId = @streamId) OR c.Version = 0"
+    ).WithParameter("streamId", streamId.ToString());
+    var documents = await dbProvider.Container.GetItemQueryIterator<EventStreamDocumentEntity>(query)
+      .ReadNextAsync(cancellationToken);
+
+    var creationDocument = documents.First(x => x.Version == 0);
+    var maxVersionDocument = documents.First(x => x.Version != 0);
+
+    EventStreamIndexEntity index = new()
+    {
+      StreamId = streamId,
+      Created = creationDocument.Time,
+      Version = maxVersionDocument.Version,
+      NextVersion = maxVersionDocument.Version + 1,
+      Updated = maxVersionDocument.Time,
+      TargetType = creationDocument.TargetType
+    };
+
+    return await dbProvider
+      .Container
+      .CreateItemAsync(index, new PartitionKey(streamId.ToString()), cancellationToken: cancellationToken)
+      .ConfigureAwait(false);
   }
 
   public async Task<IEventStream> CreateAsync(
--- src/Papst.EventStore.AzureCosmos/CosmosEventStoreOptions.cs
+++ src/Papst.EventStore.AzureCosmos/CosmosEventStoreOptions.cs
@@ -3,4 +3,5 @@ namespace Papst.EventStore.AzureCosmos;
 public class CosmosEventStoreOptions
 {
   public int ConcurrencyRetryCount { get; set; } = 3;
+  public bool BuildIndexOnNotFound { get; set; }
 }
