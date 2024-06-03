8ef7b46d2f03b4dc609d4082b44a66132a9a0ab1
Marco Papst <marco@papst.io>
2024-06-03 21:44:26 +0200

    remove compiler warnings

--- src/Papst.EventStore/Logging.cs
+++ src/Papst.EventStore/Logging.cs
@@ -3,17 +3,15 @@
 namespace Papst.EventStore;
 internal static partial class Logging
 {
-  private const string EventName = "Papst.EventStore";
-
-  [LoggerMessage(EventId = 100_010, EventName = EventName, Level = LogLevel.Debug, Message = "Resolved Event {EventName} to {EventType}")]
+  [LoggerMessage(EventId = 100_010, EventName = nameof(EventNameResolved), Level = LogLevel.Debug, Message = "Resolved Event {EventName} to {EventType}")]
   public static partial void EventNameResolved(ILogger logger, string eventName, Type eventType);
 
-  [LoggerMessage(EventId = 100_011, EventName = EventName, Level = LogLevel.Error, Message = "Event {EventName} could not be resolved, please make sure your Events are properly marked with the EventNameAttribute!")]
+  [LoggerMessage(EventId = 100_011, EventName = nameof(EventNameNotResolved), Level = LogLevel.Error, Message = "Event {EventName} could not be resolved, please make sure your Events are properly marked with the EventNameAttribute!")]
   public static partial void EventNameNotResolved(ILogger logger, string eventName);
 
-  [LoggerMessage(EventId = 100_012, EventName = EventName, Level = LogLevel.Debug, Message = "Resolved EventType for {EventName} to {EventType}")]
+  [LoggerMessage(EventId = 100_012, EventName = nameof(EventTypeResolved), Level = LogLevel.Debug, Message = "Resolved EventType for {EventName} to {EventType}")]
   public static partial void EventTypeResolved(ILogger logger, string eventName, Type eventType);
 
-  [LoggerMessage(EventId = 100_013, EventName = EventName, Level = LogLevel.Error, Message = "Event Type {EventType} could not be resolved, please make sure your Events are properly marked with the EventNameAttribute!")]
+  [LoggerMessage(EventId = 100_013, EventName = nameof(EventTypeNotResolved), Level = LogLevel.Error, Message = "Event Type {EventType} could not be resolved, please make sure your Events are properly marked with the EventNameAttribute!")]
   public static partial void EventTypeNotResolved(ILogger logger, Type eventType);
 }
